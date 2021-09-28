e27f03ad2fc2df0572d25b5bffb9cbcb4da0cffa
Soliloquiess <cyh1219@naver.com>
2021-09-28 22:30:54 +0900

    feat(yacho): ~2d Array spiral matrix

---
+++ CodeUp/C_1485.py
@@ -0,0 +1,43 @@
+n, m = map(int, input().split())
+Arr = [[0]*m for _ in range(n)]
+
+x=n*m;
+i=0;
+j=0;
+
+Xaxis = 1;
+Yaxis = 1;
+
+for k in range(1,n*m+1):
+    Arr[i][j] =x;
+    x-=1;
+    if (j <= m and Xaxis == 1 and Yaxis == 1):
+        if (j+1 < m and Arr[i][j+1] == 0):
+            j+=1;
+        else:
+            i+=1;
+            Xaxis = -1;
+    elif ( i <=n and Xaxis == -1 and Yaxis == 1):
+        if (i+1 < n and Arr[i+1][j] == 0):
+            i+=1;
+        else:
+            j-=1;
+            Yaxis =-1;
+
+    elif (j >=0 and Xaxis == -1 and Yaxis == -1):
+        if (j-1 >= 0 and Arr[i][j-1] == 0):
+            j-=1;
+        else:
+            i-=1;
+            Xaxis = 1;
+
+    elif (i >=0 and Yaxis == -1 and Xaxis == 1 ):
+        if (i-1 >= 0 and Arr[i-1][j] == 0):
+            i-=1;
+        else:
+            j+=1;
+            Yaxis = 1;
+for i in range(0,n):
+    for j in range(0,m):
+        print(Arr[i][j],end=" ");
+    print("");
