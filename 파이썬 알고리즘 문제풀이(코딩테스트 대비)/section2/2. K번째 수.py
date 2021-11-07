66f5f85bb0b2d9c33e72fe2df7c68a741036cdd7
Soliloquiess <cyh1219@naver.com>
2021-11-07 21:55:35 +0900

    feat(yacho): 인프런 알고리즘 section2 refactoring and python source add

---
+++ 파이썬 알고리즘 문제풀이(코딩테스트 대비)/section2/2. K번째 수.py
@@ -0,0 +1,22 @@
+import sys
+#sys.stdin=open("input.txt", "r")
+T=int(input())
+ans1=[]
+ans2=[]
+ans={}
+def solution(T):
+    for t in range(T): #t는 0부터 테스트 케이스 만큼 돈다
+        n, s, e, k=map(int, input().split())
+        a=list(map(int, input().split()))
+
+        a=a[s-1:e]  #첫번째꺼가 0번이므로 -1해줘야 e도 마찬가지로 e-1까지이므로
+
+        a.sort()
+
+        print("#%d %d" %(t+1, a[k-1]))
+        ans1.append(t+1);
+        ans2.append(a[k-1]);
+        ans.setdefault(t+1,a[k-1]);
+    return ans;
+
+print(solution(T));
