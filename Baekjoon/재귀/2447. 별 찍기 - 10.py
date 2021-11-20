02cd2959789069ef0259e3a87af3273cd6d7d469
Soliloquiess <cyh1219@naver.com>
2021-11-20 21:25:24 +0900

    feat(yacho):백준 재귀 add

---
+++ Baekjoon/재귀/2447. 별 찍기 - 10.py
@@ -0,0 +1,29 @@
+N = int(input())
+
+stars = ["***", "* *", "***"]
+
+cnt = 0
+while N > 3:
+    N //= 3
+    cnt += 1
+
+
+def makeStar():
+    L = len(stars)
+    newStars = []
+    for i in range(L*3):
+    	# 빈칸을 만드는 규칙
+        if i // L == 1:
+            newStars.append(stars[i % L] + " " * L + stars[i % L])
+        else:
+            newStars.append(stars[i % L]*3)
+    return newStars
+
+
+for i in range(cnt):
+    stars = makeStar()
+
+for star in stars:
+    print(star)
+
+# https://leeiopd.tistory.com/m/297
