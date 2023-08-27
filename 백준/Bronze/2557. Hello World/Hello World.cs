6fc15c6edc58d42aacaa91c9963ff97ef3d62d42
Soliloquiess <cyh1219@naver.com>
2023-08-27 18:38:48 +0900

    [Bronze V] Title: Hello World, Time: 52 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Main
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
