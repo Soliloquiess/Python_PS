23453ea85ae19a1d42e448d406847ffbd76f59f3
Soliloquiess <cyh1219@naver.com>
2024-06-03 01:39:55 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+num = list(map(int, input().split()))
+# num = map(int, input().split())
+v = int(input())
+
+print(num.count(v))
+
+# 입력 받기
+#N = int(input())  # 첫째 줄: 정수의 개수 N
+#numbers = list(map(int, input().split()))  # 둘째 줄: N개의 정수
+#v = int(input())  # 셋째 줄: 찾으려고 하는 정수 v
+
+# 정수 v의 개수 세기
+#count_v = numbers.count(v)
+
+# 결과 출력
+#print(count_v)
