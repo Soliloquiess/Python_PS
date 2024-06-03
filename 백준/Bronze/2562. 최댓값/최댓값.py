5acdced77be134b7a05a693de624b86b7ed0dd45
Soliloquiess <cyh1219@naver.com>
2024-06-04 01:59:55 +0900

    Update 최댓값.py

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,15 +1,32 @@
+# 입력 받기
+numbers = [int(input()) for _ in 9]
 
-List =[];
-max=0;
-max_index=0;
-for i in range(9):
-    i = int(input())
-    List.append(i)
+# 초기 최댓값 설정
+max_value = numbers[0]
+max_index = 1
+
+# 최댓값과 그 인덱스 찾기
+for i in range(1, 9):
+    if numbers[i] > max_value:
+        max_value = numbers[i]
+        max_index = i + 1  # 인덱스는 1부터 시작
+
+# 결과 출력
+print(max_value)
+print(max_index)
 
 
-for i in range(0,9):
-    if(List[i] > max):
-        max= List[i];
-        max_index=i+1;
-print(max,sep="");
-print(max_index);
+# List =[];
+# max=0;
+# max_index=0;
+# for i in range(9):
+#     i = int(input())
+#     List.append(i)
+# 
+# 
+# for i in range(0,9):
+#     if(List[i] > max):
+#         max= List[i];
+#         max_index=i+1;
+# print(max,sep="");
+# print(max_index);
