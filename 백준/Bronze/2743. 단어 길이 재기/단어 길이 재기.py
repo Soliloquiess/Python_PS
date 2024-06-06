5f7ba406882f9db8420b606fc1392064277e3984
Soliloquiess <cyh1219@naver.com>
2024-06-07 02:29:54 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.py
@@ -0,0 +1,17 @@
+# 단어 입력 받기
+word = input()
+
+# 단어의 길이 출력
+print(len(word))
+
+
+# 단어 입력 받기
+#word = input()
+
+# 단어 길이 계산 (len 함수 사용하지 않음)
+#length = 0
+#for _ in word:
+#    length += 1
+
+# 단어의 길이 출력
+#print(length)
