ca0223ef3e3eea353564292bd037ee78417cd94b
Soliloquiess <cyh1219@naver.com>
2024-06-09 19:12:42 +0900

    [Bronze II] Title: 상수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,4 +1,18 @@
-A, B = map(str, input().split())
-A = int(A[2]+A[1]+A[0])
-B = int(B[2]+B[1]+B[0])
-print(max(A, B))
+# 입력 받기
+A, B = input().split()
+
+# 숫자를 거꾸로 읽기
+reversed_A = int(A[::-1])
+reversed_B = int(B[::-1])
+
+# 큰 숫자 찾기
+if reversed_A > reversed_B:
+    print(reversed_A)
+else:
+    print(reversed_B)
+
+
+# A, B = map(str, input().split())
+# A = int(A[2]+A[1]+A[0])
+# B = int(B[2]+B[1]+B[0])
+# print(max(A, B))
