6f2ca040bc45e03454fba81344f037fc6434aa3a
Soliloquiess <cyh1219@naver.com>
2024-07-03 00:54:12 +0900

    [Silver V] Title: 숫자 카드, Time: 476 ms, Memory: 125620 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,12 +1,8 @@
-# 입력 받기
-import sys
-input = sys.stdin.read
-data = input().split()
 
-n = int(data[0])
-cards = set(map(int, data[1:n+1]))
-m = int(data[n+1])
-queries = list(map(int, data[n+2:]))
+n = int(input())  # 첫 번째 줄: 상근이가 가진 숫자 카드의 개수
+cards = set(map(int, input().split()))  # 두 번째 줄: 상근이가 가진 숫자 카드 목록
+m = int(input())  # 세 번째 줄: 확인할 숫자의 개수
+queries = list(map(int, input().split()))  # 네 번째 줄: 확인할 숫자 목록
 
 # 결과를 저장할 리스트
 results = []
