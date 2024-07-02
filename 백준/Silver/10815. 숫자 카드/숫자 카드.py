a7a3358460e324d83915c3e08c06719eb63b2b79
Soliloquiess <cyh1219@naver.com>
2024-07-03 00:53:45 +0900

    [Silver V] Title: 숫자 카드, Time: 500 ms, Memory: 196776 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -4,29 +4,19 @@ input = sys.stdin.read
 data = input().split()
 
 n = int(data[0])
-cards = list(map(int, data[1:n+1]))
+cards = set(map(int, data[1:n+1]))
 m = int(data[n+1])
 queries = list(map(int, data[n+2:]))
-
-# 상근이가 가진 숫자 카드를 정렬
-cards.sort()
 
 # 결과를 저장할 리스트
 results = []
 
-# 이진 탐색 직접 구현
+# 집합을 사용하여 존재 여부 확인
 for query in queries:
-    left, right = 0, len(cards) - 1
-    found = 0
-    while left <= right:
-        mid = (left + right) // 2
-        if cards[mid] == query:
-            found = 1
-            break
-        elif cards[mid] < query:
-            left = mid + 1
-        else:
-            right = mid - 1
-    results.append(found)
+    if query in cards:
+        results.append(1)
+    else:
+        results.append(0)
 
+# 결과 출력
 print(" ".join(map(str, results)))
