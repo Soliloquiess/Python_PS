35cb296846e335d005b616f9dc83d31104b6a72b
Soliloquiess <cyh1219@naver.com>
2024-09-05 03:01:56 +0900

    [Silver V] Title: 창문 닫기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.py
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.py
@@ -1,15 +1,10 @@
-def open_windows_count(N):
-    # 직접 제곱근을 구하는 이분 탐색 알고리즘
-    left, right = 0, N
-    while left <= right:
-        mid = (left + right) // 2
-        if mid * mid == N:
-            return mid
-        elif mid * mid < N:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return right
+def count_open_windows(N):
+    count = 0
+    i = 1
+    while i * i <= N:
+        count += 1
+        i += 1
+    return count
 
 N = int(input())
-print(open_windows_count(N))  
+print(count_open_windows(N))
