0c2c0ce21c729c3cacfbb511a5f54e097b2ca806
Soliloquiess <cyh1219@naver.com>
2025-03-30 17:40:10 +0900

    [level 0] Title: 코드 처리하기, Time: 11.97 ms, Memory: 9.85 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -10,3 +10,29 @@ def solution(code):
                 answer.append(char)
 
     return ''.join(answer) if answer else 'EMPTY'
+
+
+# def solution(code):
+#     mode = 0  # 시작 시 mode는 0
+#     ret = ""  # 최종 결과를 저장할 변수
+
+#     for idx, char in enumerate(code):
+#         if mode == 0:
+#             if char != "1":
+#                 if idx % 2 == 0:  # mode가 0이고, idx가 짝수일 때만
+#                     ret += char  # 문자 추가
+#             else:
+#                 mode = 1  # mode를 1로 변경
+
+#         elif mode == 1:
+#             if char != "1":
+#                 if idx % 2 == 1:  # mode가 1이고, idx가 홀수일 때만
+#                     ret += char  # 문자 추가
+#             else:
+#                 mode = 0  # mode를 0으로 변경
+
+#     return ret if ret != "" else "EMPTY"  # 결과가 빈 문자열이면 "EMPTY" 반환
+
+
+# # 테스트
+# print(solution("abc1abc1abc"))  # "acbac"
