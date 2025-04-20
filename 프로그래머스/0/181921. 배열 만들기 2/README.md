c049fb2a356d8e7d6ee26c32dffca87e14f4b5e7
Soliloquiess <cyh1219@naver.com>
2025-04-21 00:40:41 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.06 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/README.md
+++ 프로그래머스/0/181921. 배열 만들기 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 2 - 181921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181921) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181921#) 
 
 ### 성능 요약
 
-메모리: 9.36 MB, 시간: 0.10 ms
+메모리: 9.22 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 21일 00:36:42
+2025년 04월 21일 00:40:39
 
 ### 문제 설명
 
--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,19 +1,42 @@
 def solution(l, r):
     from collections import deque
     
-    result = []
-    queue = deque(['5'])  
+    result = []  # 결과를 저장할 리스트
+    queue = deque(['5'])  # 큐에 시작 숫자 '5'를 넣음
 
-    while queue:
-        num_str = queue.popleft()
-        num = int(num_str)
-        
-        if num > r:
+    while queue:  # 큐가 비어있지 않으면 계속 반복
+        num_str = queue.popleft()  # 큐에서 첫 번째 원소를 꺼냄
+        num = int(num_str)  # 문자열을 정수로 변환
+
+        if num > r:  # 만약 숫자가 r보다 크면 처리할 필요 없음
             continue
-        if num >= l:
+        if num >= l:  # 숫자가 l 이상이면 결과에 추가
             result.append(num)
-        
-        queue.append(num_str + '0')
-        queue.append(num_str + '5')
-    
+
+        # 새로운 숫자들을 큐에 추가 (자릿수 늘리기)
+        queue.append(num_str + '0')  # '0'을 추가한 새로운 숫자
+        queue.append(num_str + '5')  # '5'를 추가한 새로운 숫자
+
+    # 결과가 있으면 정렬해서 반환, 없으면 [-1] 반환
+    return sorted(result) if result else [-1]
+
+
+def solution(l, r):
+    result = []  # 결과를 저장할 리스트
+    queue = ['5']  # 큐에 시작 숫자 '5'를 넣음
+
+    while queue:  # 큐가 비어있지 않으면 계속 반복
+        num_str = queue.pop(0)  # 큐에서 첫 번째 원소를 꺼냄 (리스트의 경우 pop(0))
+        num = int(num_str)  # 문자열을 정수로 변환
+
+        if num > r:  # 만약 숫자가 r보다 크면 처리할 필요 없음
+            continue
+        if num >= l:  # 숫자가 l 이상이면 결과에 추가
+            result.append(num)
+
+        # 새로운 숫자들을 큐에 추가 (자릿수 늘리기)
+        queue.append(num_str + '0')  # '0'을 추가한 새로운 숫자
+        queue.append(num_str + '5')  # '5'를 추가한 새로운 숫자
+
+    # 결과가 있으면 정렬해서 반환, 없으면 [-1] 반환
     return sorted(result) if result else [-1]
