a5a7f183474430c251c91fec7a6dfcf2a029f1ae
Soliloquiess <cyh1219@naver.com>
2025-07-20 02:10:51 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.py
@@ -0,0 +1,11 @@
+def solution(num_list, n):
+    return num_list[::n]
+
+
+#def solution(num_list, n):
+#    answer = []
+#    i = 0
+#    while i < len(num_list):
+#        answer.append(num_list[i])
+#        i += n
+#    return answer
