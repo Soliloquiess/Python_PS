6e806832960efa62326307ded0415dedb8869748
Soliloquiess <cyh1219@naver.com>
2025-08-06 10:35:44 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.py
@@ -0,0 +1,11 @@
+def solution(myString):
+    return myString.lower()
+
+#def solution(myString):
+#    result = ''
+#    for ch in myString:
+#        if 'A' <= ch <= 'Z':
+#            result += chr(ord(ch) + 32)  # 대문자를 소문자로
+#        else:
+#            result += ch  # 이미 소문자면 그대로
+#    return result
