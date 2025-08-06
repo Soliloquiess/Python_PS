9ae7ea025face0b5d53d04540f1945b9a6d5b6de
Soliloquiess <cyh1219@naver.com>
2025-08-06 10:34:40 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.00 ms, Memory: 9.05 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.py
@@ -0,0 +1,12 @@
+def solution(myString):
+    return myString.upper()
+
+#def solution(myString):
+#    result = ''
+#    for ch in myString:
+#        if 'a' <= ch <= 'z':
+#            result += chr(ord(ch) - 32)  # 소문자를 대문자로
+#        else:
+#            result += ch  # 
+#    return result
+
