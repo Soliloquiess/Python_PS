2317e6e7bda397dd80ddd945fc0466144c32e4bd
Soliloquiess <cyh1219@naver.com>
2025-08-17 17:16:43 +0900

    [level 0] Title: 부분 문자열, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.py
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.py
@@ -1,13 +1,11 @@
 def solution(str1, str2):
-    if str2.find(str1) != -1:   # 위치가 -1이 아니면 존재
+    if str2.find(str1) != -1:  
         return 1
     else:
         return 0
 
-
-
-#def solution(str1, str2):
-#    if str1 in str2:   # 부분 문자열인지 확인
-#        return 1
-#    else:
-        return 0
+#def solution(array):
+#    total = 0
+#    for num in array:
+#        total += str(num).count("7")
+#    return total
