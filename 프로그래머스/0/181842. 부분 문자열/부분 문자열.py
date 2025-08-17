1d619392e2d4e3b466c063094bb8817cfab9eff9
Soliloquiess <cyh1219@naver.com>
2025-08-17 13:56:21 +0900

    [level 0] Title: 부분 문자열, Time: 0.00 ms, Memory: 9.08 MB -BaekjoonHub

---
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.py
@@ -0,0 +1,13 @@
+def solution(str1, str2):
+    if str2.find(str1) != -1:   # 위치가 -1이 아니면 존재
+        return 1
+    else:
+        return 0
+
+
+
+#def solution(str1, str2):
+#    if str1 in str2:   # 부분 문자열인지 확인
+#        return 1
+#    else:
+        return 0
