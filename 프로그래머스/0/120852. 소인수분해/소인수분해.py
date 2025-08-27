055350ba08a0c2def5eb528786a7c284c3debf1e
Soliloquiess <cyh1219@naver.com>
2025-08-28 00:46:03 +0900

    [level 0] Title: 소인수분해, Time: 0.84 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = []
+    divisor = 2
+    while n > 1:              
+        if n % divisor == 0:  
+            answer.append(divisor)
+            n //= divisor     
+        else:
+            divisor += 1     
+    return sorted(list(set(answer)))
