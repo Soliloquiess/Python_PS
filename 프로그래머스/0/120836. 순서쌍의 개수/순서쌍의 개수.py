444c8b4c4e90c64abee7952b13d0433231c44cd8
Soliloquiess <cyh1219@naver.com>
2025-09-02 03:51:33 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.64 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -0,0 +1,17 @@
+def solution(n):
+    count = 0
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            if i * i == n:
+                count += 1
+            else:
+                count += 2
+    return count
+
+
+#def solution(n):
+#    count = 0
+#    for i in range(1, n + 1):
+#        if n % i == 0:
+#            count += 1
+#    return count
